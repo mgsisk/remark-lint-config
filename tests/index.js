450b9d8f98c6cf1557c3c73d2485e1525f6731e2
Michael Sisk <me@mgsisk.com>
2018-12-08 14:11:14 -0500

    Simplify test messages

--- tests/index.js
+++ tests/index.js
@@ -1,19 +1,19 @@
 const tap = require('tap')
-const config = require('../index.js')
 const remark = require('remark') // eslint-disable-line import/no-extraneous-dependencies
+const config = require('../index.js')
 
 remark().use(config)
   .process('A clean Markdown file.\n', (err, file)=> {
     tap.equal(err, null, "It doesn't produce errors during processing")
-    tap.equal(file.messages.length, 0, 'It finds no errors in a valid Markdown file')
+    tap.equal(file.messages.length, 0, 'It passes valid Markdown')
   })
 
 remark().use(config)
   .process('A dirty Markdown file.', (_err, file)=> {
-    tap.equal(file.messages.length, 1, 'It finds errors in an invalid Markdown file')
+    tap.equal(file.messages.length, 1, 'It lints invalid Markdown')
   })
 
 remark().use(config)
   .process('---\nfront: 1\n---\n\n# Heading\n\nMarkdown.\n', (_err, file)=> {
-    tap.equal(file.messages.length, 0, 'It ignores front matter in Markdown files')
+    tap.equal(file.messages.length, 0, 'It ignores front matter in Markdown')
   })
