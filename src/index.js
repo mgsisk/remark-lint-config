63fafdb06cedbfef54d7fba5cd580be29bd40e39
Michael Sisk <me@mgsisk.com>
2020-04-11 22:24:47 -0400

    Update configuration

--- src/index.js
+++ src/index.js
@@ -1,59 +1,48 @@
-/* eslint-disable import/max-dependencies */
-
 /**
  * Remark-lint configuration for general writing.
  *
- * @type {Object}
+ * @type {object}
  */
 module.exports = {
   plugins: [
+    /* eslint-disable import/max-dependencies */
     require('remark-frontmatter'),
 
     // ----- Presets -----------------------------------------------------------
-    require('remark-preset-lint-consistent'),
+    require('remark-preset-lint-markdown-style-guide'),
     require('remark-preset-lint-recommended'),
+    require('remark-preset-lint-consistent'),
 
     // ----- Overrides ---------------------------------------------------------
-    [require('remark-lint-list-item-indent'), 'space'], // eslint-disable-line import/no-extraneous-dependencies
+    /* eslint-disable import/no-extraneous-dependencies */
+    [require('remark-lint-list-item-indent'), 'space'],
+    [require('remark-lint-list-item-spacing'), false],
+    [require('remark-lint-no-duplicate-headings'), false],
+    [require('remark-lint-no-file-name-irregular-characters'), '\\.a-zA-Z0-9-_'],
+    [require('remark-lint-no-file-name-mixed-case'), false],
+    [require('remark-lint-unordered-list-marker-style'), 'consistent'],
+    /* eslint-enable import/no-extraneous-dependencies */
+
 
     // ----- Built-In ----------------------------------------------------------
     require('remark-lint-checkbox-content-indent'),
-    require('remark-lint-definition-spacing'),
-    require('remark-lint-fenced-code-flag'),
-    require('remark-lint-file-extension'),
-    require('remark-lint-heading-increment'),
     require('remark-lint-linebreak-style'),
-    require('remark-lint-maximum-heading-length'),
-    require('remark-lint-maximum-line-length'),
-    require('remark-lint-no-consecutive-blank-lines'),
     require('remark-lint-no-duplicate-headings-in-section'),
-    require('remark-lint-no-emphasis-as-heading'),
     require('remark-lint-no-empty-url'),
-    require('remark-lint-no-file-name-articles'),
-    require('remark-lint-no-file-name-consecutive-dashes'),
-    [require('remark-lint-no-file-name-irregular-characters'), '\\.a-zA-Z0-9-_'],
-    require('remark-lint-no-file-name-outer-dashes'),
     require('remark-lint-no-heading-indent'),
     require('remark-lint-no-heading-like-paragraph'),
-    require('remark-lint-no-heading-punctuation'),
     require('remark-lint-no-html'),
-    require('remark-lint-no-multiple-toplevel-headings'),
     require('remark-lint-no-paragraph-content-indent'),
     require('remark-lint-no-reference-like-url'),
-    require('remark-lint-no-shell-dollars'),
-    require('remark-lint-no-table-indentation'),
     require('remark-lint-no-tabs'),
     require('remark-lint-no-unneeded-full-reference-image'),
     require('remark-lint-no-unneeded-full-reference-link'),
-    require('remark-lint-ordered-list-marker-value'),
-    require('remark-lint-table-pipe-alignment'),
-    require('remark-lint-table-pipes'),
-    require('remark-lint-unordered-list-marker-style'),
 
     // ----- External ----------------------------------------------------------
     require('remark-lint-heading-whitespace'),
     require('remark-lint-no-empty-sections'),
     require('remark-lint-no-url-trailing-slash'),
     require('remark-lint-write-good'),
+    /* eslint-enable import/max-dependencies */
   ],
 }
