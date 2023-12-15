05aada6094c844ab8610e0c9e63b9115b1828f91
Michael Sisk <me@mgsisk.com>
2023-12-15 18:59:52 -0500

    Update index.js

--- test/index.js
+++ test/index.js
@@ -1,19 +1,26 @@
-const tap = require('tap')
-const remark = require('remark') // eslint-disable-line import/no-extraneous-dependencies
-const config = require('../src')
+import tap from "tap";
+import { remark } from "remark";
+import config from "../src/index.js"; // eslint-disable-line import/extensions, import/no-relative-parent-imports
 
-remark().use(config)
-  .process('A clean Markdown file.\n', (err, file)=> {
-    tap.equal(err, null, "It doesn't produce errors during processing")
-    tap.equal(file.messages.length, 0, 'It passes valid Markdown')
-  })
+remark()
+  .use(config)
+  .process("A clean Markdown file.\n", (err, file) => {
+    tap.equal(
+      true,
+      typeof err === "undefined",
+      "It doesn't produce errors during processing",
+    );
+    tap.equal(file.messages.length, 0, "It passes valid Markdown");
+  });
 
-remark().use(config)
-  .process('A dirty Markdown file.', (_err, file)=> {
-    tap.equal(file.messages.length, 1, 'It lints invalid Markdown')
-  })
+remark()
+  .use(config)
+  .process("A dirty Markdown file.", (_err, file) => {
+    tap.equal(file.messages.length, 1, "It lints invalid Markdown");
+  });
 
-remark().use(config)
-  .process('---\nfront: 1\n---\n\n# Heading\n\nMarkdown.\n', (_err, file)=> {
-    tap.equal(file.messages.length, 0, 'It ignores front matter in Markdown')
-  })
+remark()
+  .use(config)
+  .process("---\nfront: 1\n---\n\n# Heading\n\nMarkdown.\n", (_err, file) => {
+    tap.equal(file.messages.length, 0, "It ignores front matter in Markdown");
+  });
